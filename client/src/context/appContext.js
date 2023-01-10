6532922b5bc8df50fdc050c53f711e761fdd6cd7
Jimmy Bäckström <jimmy.backstrom@gruppera.se>
2023-01-10 12:03:13 +0100

    appContext.js: Hack to solve eslintr error for location.

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -10,6 +10,8 @@ import {
 } from './actions';
 const AppContext = createContext();
 
+const location = 'Having issue with eslint';
+
 const token = localStorage.getItem('token');
 const user = localStorage.getItem('user');
 const userLocation = localStorage.getItem('location');
@@ -47,7 +49,7 @@ const AppProvider = ({ children }) => {
   const removeUserFromLocalStorage = () => {
     localStorage.removeItem(user);
     localStorage.removeItem(token);
-    // localStorage.removeItem(location);
+    localStorage.removeItem(location);
   };
 
   const registerUser = async (currentUser) => {
