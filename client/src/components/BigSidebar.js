18fbfe9413c9cf201a47a41d320768215aee1232
Jimmy Bäckström <jimmy.backstrom@gruppera.se>
2023-01-13 15:25:48 +0100

    BigSidebar, toggle sidebar on bigger screens.

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,10 +1,26 @@
+import { useAppContext } from '../context/appContext';
+import NavLinks from './NavLinks';
+import Logo from '../components/Logo';
 import Wrapper from '../assets/wrappers/BigSidebar';
 
 const BigSidebar = () => {
+  const { showSidebar } = useAppContext();
   return (
     <Wrapper>
-      <h1>BigSidebar</h1>
+      <div
+        className={
+          showSidebar ? 'sidebar-container ' : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
+
 export default BigSidebar;
