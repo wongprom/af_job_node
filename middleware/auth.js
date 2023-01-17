bacc4ac7af4460698d8b1e82959b43c6d60f8b73
Jimmy Bäckström <jimmy.backstrom@gruppera.se>
2023-01-17 15:15:31 +0100

    auth.js : Remove out-commented code.

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,8 +10,6 @@ const auth = async (req, res, next) => {
   const token = authHeader.split(' ')[1];
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
-    console.log('payload', payload);
-    // req.user = payload;
     req.user = { userId: payload.userId };
     next();
   } catch (error) {
