1c319a9de0140990b165de2bc0f2830c65541758
Jimmy Bäckström <jimmy.backstrom@gruppera.se>
2023-01-24 10:27:44 +0100

    Change width in bar chart.

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -21,7 +21,7 @@ const BarChartComponent = ({ data }) => {
         <XAxis dataKey="date" />
         <YAxis allowDecimals={false} />
         <Tooltip />
-        <Bar dataKey="count" fill="#2cb1bc" barSize={75} />
+        <Bar dataKey="count" fill="#2cb1bc" barSize={50} />
       </BarChart>
     </ResponsiveContainer>
   );
