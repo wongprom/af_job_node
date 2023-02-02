83969737662a8dd01bd2aef4ac172fed35813b46
Jimmy Bäckström <jimmy.backstrom@gruppera.se>
2023-02-02 10:31:11 +0100

    Landing page navigate if user exist.

--- README.md
+++ README.md
@@ -8180,6 +8180,68 @@ export default ProtectedRoute;
 
 </details>
 
+## Minor Landing Page fix
+
+<details>
+  <summary>Small navigation fix if user exist </summary><br>
+
+Update Landing imports and return
+
+###### Root/client/src/pages/Landing.js
+
+```js
+import { Navigate } from 'react-router-dom';
+{
+  /* < -- */
+}
+import { useAppContext } from '../context/appContext';
+{
+  /* < -- */
+}
+
+const Landing = () => {
+  const { user } = useAppContext();
+  {
+    /* < -- */
+  }
+
+  return (
+    <React.Fragment>
+      {' '}
+      {/* < -- */}
+      {user && <Navigate to="/" />} {/* < -- */}
+      <Wrapper>
+        <nav>
+          <Logo />
+        </nav>
+        <div className="container page">
+          <div className="info">
+            <h1>
+              job<span> tracking </span>app
+            </h1>
+            <p>
+              I'm baby live-edge woke you probably haven't heard of them,
+              tousled kinfolk schlitz enamel pin. Letterpress gluten-free sus
+              ugh. Adaptogen sus beard pug readymade vaporware subway tile
+              whatever taxidermy plaid cray literally artisan next level.
+            </p>
+            <Link to="/register" className="btn btn-hero">
+              Login/register
+            </Link>
+          </div>
+          <img src={main} alt="job hunt" className="img main-img" />
+        </div>
+      </Wrapper>
+    </React.Fragment>
+  );
+};
+
+export default Landing;
+```
+
+---
+
+</details>
 <br>
 <br>
 <br>
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,32 +1,38 @@
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import main from '../assets/images/main.svg';
 import Wrapper from '../assets/wrappers/LandingPage';
 import { Logo } from '../components';
+import { useAppContext } from '../context/appContext';
 
 const Landing = () => {
+  const { user } = useAppContext();
+
   return (
-    <Wrapper>
-      <nav>
-        <Logo />
-      </nav>
-      <div className="container page">
-        <div className="info">
-          <h1>
-            job<span> tracking </span>app
-          </h1>
-          <p>
-            I'm baby live-edge woke you probably haven't heard of them, tousled
-            kinfolk schlitz enamel pin. Letterpress gluten-free sus ugh.
-            Adaptogen sus beard pug readymade vaporware subway tile whatever
-            taxidermy plaid cray literally artisan next level.
-          </p>
-          <Link to="/register" className="btn btn-hero">
-            Login/register
-          </Link>
+    <React.Fragment>
+      {user && <Navigate to="/" />}
+      <Wrapper>
+        <nav>
+          <Logo />
+        </nav>
+        <div className="container page">
+          <div className="info">
+            <h1>
+              job<span> tracking </span>app
+            </h1>
+            <p>
+              I'm baby live-edge woke you probably haven't heard of them,
+              tousled kinfolk schlitz enamel pin. Letterpress gluten-free sus
+              ugh. Adaptogen sus beard pug readymade vaporware subway tile
+              whatever taxidermy plaid cray literally artisan next level.
+            </p>
+            <Link to="/register" className="btn btn-hero">
+              Login/register
+            </Link>
+          </div>
+          <img src={main} alt="job hunt" className="img main-img" />
         </div>
-        <img src={main} alt="job hunt" className="img main-img" />
-      </div>
-    </Wrapper>
+      </Wrapper>
+    </React.Fragment>
   );
 };
 
